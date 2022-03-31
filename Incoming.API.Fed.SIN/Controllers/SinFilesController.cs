9e18db37e32cfc57573d73f8b5824f8d200fc518
DenSarJus <dsarrazi@justice.gc.ca>
2022-03-31 13:51:02 -0400

    Fixed root path for Sin application APIs

--- Incoming.API.Fed.SIN/Controllers/SinFilesController.cs
+++ Incoming.API.Fed.SIN/Controllers/SinFilesController.cs
@@ -23,12 +23,12 @@ namespace Incoming.API.Fed.SIN.Controllers
     {
         [HttpPost]
         public ActionResult ProcessSINFile([FromQuery] string fileName,
-                                              [FromServices] IFileAuditRepository fileAuditDB,
-                                              [FromServices] IFileTableRepository fileTableDB,
-                                              [FromServices] IMailServiceRepository mailService,
-                                              [FromServices] IFlatFileSpecificationRepository flatFileSpecs,
-                                              [FromServices] IOptions<ProvincialAuditFileConfig> auditConfig,
-                                              [FromServices] IOptions<ApiConfig> apiConfig)
+                                           [FromServices] IFileAuditRepository fileAuditDB,
+                                           [FromServices] IFileTableRepository fileTableDB,
+                                           [FromServices] IMailServiceRepository mailService,
+                                           [FromServices] IFlatFileSpecificationRepository flatFileSpecs,
+                                           [FromServices] IOptions<ProvincialAuditFileConfig> auditConfig,
+                                           [FromServices] IOptions<ApiConfig> apiConfig)
         {
             string flatFileContent;
             using (var reader = new StreamReader(Request.Body, Encoding.UTF8))
@@ -42,7 +42,7 @@ namespace Incoming.API.Fed.SIN.Controllers
             if (fileName.ToUpper().EndsWith(".XML"))
                 fileName = fileName[0..^4]; // remove .XML extension
 
-            var apiHelper = new APIBrokerHelper(apiConfig.Value.TracingRootAPI, "", "");
+            var apiHelper = new APIBrokerHelper(apiConfig.Value.ApplicationRootAPI, "", "");
 
             var apis = new APIBrokerList
             {
