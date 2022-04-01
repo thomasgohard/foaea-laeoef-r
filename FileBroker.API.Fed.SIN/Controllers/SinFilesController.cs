0e6dfca2f5cf18b978a8a8dc89f2035f4f2e90fd
DenSarJus <dsarrazi@justice.gc.ca>
2022-04-01 14:46:07 -0400

    Use file scope namespaces

--- FileBroker.API.Fed.SIN/Controllers/SinFilesController.cs
+++ FileBroker.API.Fed.SIN/Controllers/SinFilesController.cs
@@ -11,96 +11,95 @@ using System.IO;
 using System.Linq;
 using System.Text;
 
-namespace Incoming.API.Fed.SIN.Controllers
+namespace FileBroker.API.Fed.SIN.Controllers;
+
+[Route("api/v1/[controller]")]
+[ApiController]
+public class SinFilesController : ControllerBase
 {
-    [Route("api/v1/[controller]")]
-    [ApiController]
-    public class SinFilesController : ControllerBase
+    [HttpGet]
+    public IActionResult GetFile([FromServices] IFileTableRepository fileTable)
     {
-        [HttpGet]
-        public IActionResult GetFile([FromServices] IFileTableRepository fileTable)
+        string fileContent = LoadLatestFederalSinFile(fileTable, out string lastFileName);
+
+        if (fileContent == null)
+            return NotFound();
+
+        byte[] result = Encoding.UTF8.GetBytes(fileContent);
+
+        return File(result, "text/plain", lastFileName);
+    }
+
+    private static string LoadLatestFederalSinFile(IFileTableRepository fileTable, out string lastFileName)
+    {
+        var fileTableData = fileTable.GetFileTableDataForCategory("SINOUT")
+                                     .FirstOrDefault(m => m.Active.HasValue && m.Active.Value);
+
+        var fileLocation = fileTableData.Path;
+        int lastFileCycle = fileTableData.Cycle;
+
+        int fileCycleLength = 3; // TODO: should come from FileTable
+
+        var lifeCyclePattern = new string('0', fileCycleLength);
+        string lastFileCycleString = lastFileCycle.ToString(lifeCyclePattern);
+        lastFileName = $"{fileTableData.Name}.{lastFileCycleString}";
+
+        string fullFilePath = $"{fileLocation}{lastFileName}";
+        if (System.IO.File.Exists(fullFilePath))
+            return System.IO.File.ReadAllText(fullFilePath);
+        else
+            return null;
+
+    }
+
+    [HttpPost]
+    public ActionResult ProcessSINFile([FromQuery] string fileName,
+                                       [FromServices] IFileAuditRepository fileAuditDB,
+                                       [FromServices] IFileTableRepository fileTableDB,
+                                       [FromServices] IMailServiceRepository mailService,
+                                       [FromServices] IFlatFileSpecificationRepository flatFileSpecs,
+                                       [FromServices] IOptions<ProvincialAuditFileConfig> auditConfig,
+                                       [FromServices] IOptions<ApiConfig> apiConfig)
+    {
+        string flatFileContent;
+        using (var reader = new StreamReader(Request.Body, Encoding.UTF8))
         {
-            string fileContent = LoadLatestFederalSinFile(fileTable, out string lastFileName);
-
-            if (fileContent == null)
-                return NotFound();
-
-            byte[] result = Encoding.UTF8.GetBytes(fileContent);
-
-            return File(result, "text/plain", lastFileName);
+            flatFileContent = reader.ReadToEndAsync().Result;
         }
 
-        private static string LoadLatestFederalSinFile(IFileTableRepository fileTable, out string lastFileName)
+        if (string.IsNullOrEmpty(fileName))
+            return UnprocessableEntity("Missing fileName");
+
+        if (fileName.ToUpper().EndsWith(".XML"))
+            fileName = fileName[0..^4]; // remove .XML extension
+
+        var apiHelper = new APIBrokerHelper(apiConfig.Value.FoaeaApplicationRootAPI, "", "");
+
+        var apis = new APIBrokerList
         {
-            var fileTableData = fileTable.GetFileTableDataForCategory("SINOUT")
-                                         .FirstOrDefault(m => m.Active.HasValue && m.Active.Value);
+            SinAPIBroker = new SinAPIBroker(apiHelper),
+            ApplicationEventAPIBroker = new ApplicationEventAPIBroker(apiHelper),
+            ApplicationAPIBroker = new ApplicationAPIBroker(apiHelper)
+        };
 
-            var fileLocation = fileTableData.Path;
-            int lastFileCycle = fileTableData.Cycle;
-
-            int fileCycleLength = 3; // TODO: should come from FileTable
-
-            var lifeCyclePattern = new string('0', fileCycleLength);
-            string lastFileCycleString = lastFileCycle.ToString(lifeCyclePattern);
-            lastFileName = $"{fileTableData.Name}.{lastFileCycleString}";
-
-            string fullFilePath = $"{fileLocation}{lastFileName}";
-            if (System.IO.File.Exists(fullFilePath))
-                return System.IO.File.ReadAllText(fullFilePath);
-            else
-                return null;
-
-        }
-
-        [HttpPost]
-        public ActionResult ProcessSINFile([FromQuery] string fileName,
-                                           [FromServices] IFileAuditRepository fileAuditDB,
-                                           [FromServices] IFileTableRepository fileTableDB,
-                                           [FromServices] IMailServiceRepository mailService,
-                                           [FromServices] IFlatFileSpecificationRepository flatFileSpecs,
-                                           [FromServices] IOptions<ProvincialAuditFileConfig> auditConfig,
-                                           [FromServices] IOptions<ApiConfig> apiConfig)
+        var repositories = new RepositoryList
         {
-            string flatFileContent;
-            using (var reader = new StreamReader(Request.Body, Encoding.UTF8))
-            {
-                flatFileContent = reader.ReadToEndAsync().Result;
-            }
+            FlatFileSpecs = flatFileSpecs,
+            FileAudit = fileAuditDB,
+            FileTable = fileTableDB,
+            MailServiceDB = mailService
+        };
 
-            if (string.IsNullOrEmpty(fileName))
-                return UnprocessableEntity("Missing fileName");
+        var sinManager = new IncomingFederalSinManager(apis, repositories);
 
-            if (fileName.ToUpper().EndsWith(".XML"))
-                fileName = fileName[0..^4]; // remove .XML extension
-
-            var apiHelper = new APIBrokerHelper(apiConfig.Value.FoaeaApplicationRootAPI, "", "");
-
-            var apis = new APIBrokerList
-            {
-                SinAPIBroker = new SinAPIBroker(apiHelper),
-                ApplicationEventAPIBroker = new ApplicationEventAPIBroker(apiHelper),
-                ApplicationAPIBroker = new ApplicationAPIBroker(apiHelper)
-            };
-
-            var repositories = new RepositoryList
-            {
-                FlatFileSpecs = flatFileSpecs,
-                FileAudit = fileAuditDB,
-                FileTable = fileTableDB,
-                MailServiceDB = mailService
-            };
-
-            var sinManager = new IncomingFederalSinManager(apis, repositories);
-
-            var fileNameNoCycle = Path.GetFileNameWithoutExtension(fileName);
-            var fileTableData = fileTableDB.GetFileTableDataForFileName(fileNameNoCycle);
-            if (!fileTableData.IsLoading)
-            {
-                sinManager.ProcessFlatFile(flatFileContent, fileName);
-                return Ok("File processed.");
-            }
-            else
-                return UnprocessableEntity("File was already loading?");
+        var fileNameNoCycle = Path.GetFileNameWithoutExtension(fileName);
+        var fileTableData = fileTableDB.GetFileTableDataForFileName(fileNameNoCycle);
+        if (!fileTableData.IsLoading)
+        {
+            sinManager.ProcessFlatFile(flatFileContent, fileName);
+            return Ok("File processed.");
         }
+        else
+            return UnprocessableEntity("File was already loading?");
     }
 }
