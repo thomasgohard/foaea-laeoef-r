a3b6d70df81adc45c79aa414bf6b2a589ee59658
DenSarJus <dsarrazi@justice.gc.ca>
2022-07-22 13:31:02 -0400

    Added support for generating outgoing MEP stats file via FileBroker web app

--- FileBroker.Web/Pages/Tasks/CreateOutbound.cshtml.cs
+++ FileBroker.Web/Pages/Tasks/CreateOutbound.cshtml.cs
@@ -1,5 +1,9 @@
+using FileBroker.Business;
+using FileBroker.Data;
+using FileBroker.Data.DB;
 using FileBroker.Model;
 using FileBroker.Model.Interfaces;
+using FOAEA3.Common.Brokers;
 using FOAEA3.Common.Helpers;
 using FOAEA3.Model;
 using FOAEA3.Model.Interfaces;
@@ -12,16 +16,33 @@ namespace FileBroker.Web.Pages.Tasks
     {
 
         private IFileTableRepository FileTable { get; }
+        private IFlatFileSpecificationRepository FlatFileSpecs { get; }
+        private IOutboundAuditRepository OutboundAuditDB { get; }
+        private IErrorTrackingRepository ErrorTrackingDB { get; }
+        private IProcessParameterRepository ProcessParameterTable { get; }
+        private IMailServiceRepository MailServiceDB { get; }
         private ApiConfig ApiConfig { get; }
         private IAPIBrokerHelper APIHelper { get; }
 
         public string InfoMessage { get; set; }
+        public string ErrorMessage { get; set; }
 
         public List<FileTableData> ActiveOutgoingProcesses { get; set; }
 
-        public CreateOutboundModel(IFileTableRepository fileTable, IOptions<ApiConfig> apiConfig)
+        public CreateOutboundModel(IFileTableRepository fileTable, 
+                                   IFlatFileSpecificationRepository flatFileSpecs, 
+                                   IOutboundAuditRepository outboundAuditDB,
+                                   IErrorTrackingRepository errorTrackingDB,
+                                   IProcessParameterRepository processParameterTable,
+                                   IMailServiceRepository mailServiceDB,
+                                   IOptions<ApiConfig> apiConfig)
         {
             FileTable = fileTable;
+            FlatFileSpecs = flatFileSpecs;
+            OutboundAuditDB = outboundAuditDB;
+            ErrorTrackingDB = errorTrackingDB;
+            ProcessParameterTable = processParameterTable;
+            MailServiceDB = mailServiceDB;
             ApiConfig = apiConfig.Value;
             APIHelper = new APIBrokerHelper(currentSubmitter: "MSGBRO", currentUser: "MSGBRO");
         }
@@ -34,14 +55,40 @@ namespace FileBroker.Web.Pages.Tasks
         public void OnPostCreateFiles(int[] selectedProcesses)
         {
             // Category.In("TRCAPPOUT", "LICAPPOUT", "STATAPPOUT", "TRCOUT", "SINOUT", "LICOUT")
+            var applicationApiHelper = new APIBrokerHelper(ApiConfig.FoaeaApplicationRootAPI, currentSubmitter: "MSGBRO", currentUser: "MSGBRO");
+            var tracingApiHelper = new APIBrokerHelper(ApiConfig.FoaeaTracingRootAPI, currentSubmitter: "MSGBRO", currentUser: "MSGBRO");
+            var licenceDenialApiHelper = new APIBrokerHelper(ApiConfig.FoaeaLicenceDenialRootAPI, currentSubmitter: "MSGBRO", currentUser: "MSGBRO");
 
-            var processes = FileTable.GetAllActive().Where(m => m.Type.ToLower() == "out").ToList();
+            var apiBrokers = new APIBrokerList
+            {
+                Applications = new ApplicationAPIBroker(applicationApiHelper),
+                ApplicationEvents = new ApplicationEventAPIBroker(applicationApiHelper),
+                TracingApplications = new TracingApplicationAPIBroker(tracingApiHelper),
+                TracingResponses = new TraceResponseAPIBroker(tracingApiHelper),
+                TracingEvents = new TracingEventAPIBroker(tracingApiHelper),
+                LicenceDenialApplications = new LicenceDenialApplicationAPIBroker(licenceDenialApiHelper),
+                LicenceDenialTerminationApplications = new LicenceDenialTerminationApplicationAPIBroker(licenceDenialApiHelper),
+                LicenceDenialResponses = new LicenceDenialResponseAPIBroker(licenceDenialApiHelper),
+                LicenceDenialEvents = new LicenceDenialEventAPIBroker(licenceDenialApiHelper),
+            };
+
+            var repositories = new RepositoryList
+            {
+                FileTable = FileTable,
+                FlatFileSpecs = FlatFileSpecs,
+                OutboundAuditDB = OutboundAuditDB,
+                ErrorTrackingDB = ErrorTrackingDB,
+                ProcessParameterTable = ProcessParameterTable,
+                MailServiceDB = MailServiceDB
+            };
+
+            var processData = FileTable.GetAllActive().Where(m => m.Type.ToLower() == "out").ToList();
 
             InfoMessage = string.Empty;
             int lastItem = selectedProcesses.Last();
-            foreach (int selectedProcess in selectedProcesses)
+            foreach (int processId in selectedProcesses)
             {
-                var thisProcess = processes.First(m => m.PrcId == selectedProcess);
+                var thisProcess = processData.First(m => m.PrcId == processId);
 
                 string fileName;
                 if (thisProcess.IsXML)
@@ -49,9 +96,25 @@ namespace FileBroker.Web.Pages.Tasks
                 else
                     fileName = $"{thisProcess.Name.Trim()}.{thisProcess.Cycle,3:D3}";
 
-                InfoMessage += $"Creating  {fileName} [{selectedProcess}] in {thisProcess.Path}";
-                if (selectedProcess != lastItem)
+                InfoMessage += $"Creating  {fileName} [{processId}] in {thisProcess.Path}";
+                if (processId != lastItem)
                     InfoMessage += "; ";
+
+                switch (thisProcess.Category)
+                {
+                    case "STATAPPOUT":
+                        var manager = new OutgoingProvincialStatusManager(apiBrokers, repositories);
+                        
+                        string filePath = manager.CreateOutputFile(thisProcess.Name, out List<string> errors);
+                        if (errors.Count == 0)
+                            InfoMessage = $"Successfully created {filePath}";
+                        else
+                            foreach (var error in errors)
+                                ErrorMessage = $"Error creating {fileName}: {error}";
+                        break;
+                    default:
+                        break;
+                }
             }
         }
     }
