9a6a5555e8007a8e4d95c4f76781500ff35f0ac1
DenSarJus <dsarrazi@justice.gc.ca>
2022-11-21 10:21:02 -0500

    Removed declaration feature from Tracing since that is not implemented yet (C78)

--- FOAEA3.API.Tracing/Program.cs
+++ FOAEA3.API.Tracing/Program.cs
@@ -2,10 +2,6 @@ using FOAEA3.Business.Areas.Application;
 using FOAEA3.Common;
 using FOAEA3.Model;
 
-var builder = WebApplication.CreateBuilder(args);
-var localConfig = builder.Configuration;
-TracingManager.Declaration = localConfig.GetSection("Declaration").Get<DeclarationData>();
-
 await Startup.SetupAndRun(args);
 
 public partial class Program { }
