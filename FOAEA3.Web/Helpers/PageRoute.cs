2ece2d37f861645e7659eee0b279ae2eaf87f622
DenSarJus <dsarrazi@justice.gc.ca>
2023-03-03 13:31:25 -0500

    Refactoring of hardcoded page paths into shared constants

--- FOAEA3.Web/Helpers/PageRoute.cs
+++ FOAEA3.Web/Helpers/PageRoute.cs
@@ -3,12 +3,16 @@
     public static class PageRoute
     {
         public const string INTERCEPTION_DASHBOARD = "~/Applications/Dashboard/InterceptionDashboard";
+        public const string INTERCEPTION = "~/Applications/Interception/Interception/{0}";
         public const string INTERCEPTION_VIEW = "~/Applications/Interception/InterceptionView/{0}";
         public const string INTERCEPTION_EDIT = "~/Applications/Interception/InterceptionEdit/{0}";
         public const string INTERCEPTION_NEW = "~/Applications/Interception/InterceptionNew";
+        public const string INTERCEPTION_DATA = "~/Pages/Applications/_InterceptionData.cshtml";
 
         public const string VIEW_EVENTS = "~/Applications/ViewEvents/{0}";
 
         public const string LOGIN = "~/Security/Login";
+
+        public const string MESSAGES = "~/Pages/Shared/_MessagesPartial.cshtml";
     }
 }
