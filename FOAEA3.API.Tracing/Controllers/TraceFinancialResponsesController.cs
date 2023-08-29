6e7587c05f30d19fd226c71c8b1200b369d9b827
DenSarJus <dsarrazi@justice.gc.ca>
2023-08-29 14:27:00 -0400

    Removed the anonymous access to the PDF creation API

--- FOAEA3.API.Tracing/Controllers/TraceFinancialResponsesController.cs
+++ FOAEA3.API.Tracing/Controllers/TraceFinancialResponsesController.cs
@@ -5,7 +5,6 @@ using FOAEA3.Model;
 using FOAEA3.Model.Base;
 using FOAEA3.Model.Interfaces.Repository;
 using FOAEA3.Resources.Helpers;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Outgoing.FileCreator.Fed.Tracing;
 
@@ -28,7 +27,6 @@ namespace FOAEA3.API.Tracing.Controllers
         }
 
         [HttpGet("{id}/pdf")]
-        [AllowAnonymous]
         public async Task<ActionResult> GetCRAform([FromRoute] ApplKey id, [FromServices] IRepositories repositories,
                                                    [FromQuery] short year, [FromQuery] string form, [FromQuery] short cycle)
         {
